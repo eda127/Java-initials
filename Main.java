724b47c1b7249a1df95028f47a940629d27b9f6f
eda127 <127062@epsstudents.org>
2024-06-25 11:26:01 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+/*6/23/2024
+Rashard Michael Fregeau, Eduardo Porto De Aguiar
+Printing Initials (CSA Summer Work Part 2 - Unit 0 Project)*/
+
+class Main{
+    public static void main(String[] args){
+        //each System.out.println("") will print out exactly what is inbetween the quotation marks on a new line.
+        //because each row gets printed on a new line, it is very easy to see the design that will be printed in the code. They all line up with each other in the code
+        System.out.println("RRR         FFF    EEE EEE EEE    RRR        RRR            RRR MMM PPP");
+        System.out.println("RRR         FFF    PPP            MMM        RRR          RRR         EEE");
+        System.out.println("RRR         FFF    PPP            FFF        RRR         RRR           EEE");
+        System.out.println("RRR MMM MMM FFF    PPP PPP PPP    EEE        RRR        MMM             PPP");
+        System.out.println("RRR         FFF    PPP            PPP        RRR         MMM           DDD");
+        System.out.println("RRR         FFF    PPP            DDD        MMM          FFF         DDD");
+        System.out.println("RRR         FFF    DDD DDD DDD    DDD DDD    FFF FFF        FFF FFF DDD");
+    }
+}
